06369cd8bad2ecba67eac52711d42c7efbb2981c
renaupadhyay <renaupadhyay01@gmail.com>
2018-10-08 18:20:08 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,7 +1,31 @@
-def first_steps
-  # Write a loop that outputs "Right foot back", "Left foot back",
-  # "Right foot back", and "Stop", with sleep after each
+
+
+
+# #Create a variable called `steps` and set it equal to zero
+# * Just below this variable, write a loop that does four things:
+#   1. Increases the value of `steps` by 1
+#   2. Outputs the value of `steps`
+#   3. If the value of `steps` is divisible by 2 (remember how to use the `%`?),
+# outputs 'Left', otherwise, it should output 'Right'
+#   3. Sleeps for 0.5 seconds
+def how_many_steps
+ 
+loop do
+  steps= 0
+  if steps % 2 
+    puts "left"
+  else
+    puts "right"
+    steps= steps+1 
+  end
 end
+end
+
+
+
+how_many_steps
+    
+
 
 def a_few_more_steps
   # Write a loop that outputs the first two sets of steps in the Two-Step
